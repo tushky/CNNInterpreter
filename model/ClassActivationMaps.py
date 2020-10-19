fixed gradcam++ normalization

model/ClassActivationMaps.py
@@ -13,9 +13,8 @@ from PIL import Image
 import torchvision.transforms.functional as TF
 from scipy import ndimage
 import os
-from read_image import read_image, tensor_to_image
 import pickle
-from utils import postprocess
+from utils import postprocess, read_image
 from classes import classes as class_labels
 from torch.nn.functional import relu, softmax
 from tqdm import tqdm
@@ -49,7 +48,7 @@ class ClassActivationMaps:
         if not self.interactive:
             for hook in self.hooks:
                 hook.remove()
-                self.hooks = None
+            self.hooks = None
         # resize it to the shape of input image
         cam_map = nn.functional.interpolate(cam_map, size=(t.shape[2], t.shape[3]), mode='bilinear', align_corners=False)
         
@@ -57,7 +56,7 @@ class ClassActivationMaps:
         cam_map = cam_map.squeeze(0).squeeze(0)
 
         # Normalize
-        cam_map -= cam_map.min()
+        #cam_map -= cam_map.min()
         cam_map /= (cam_map.max() + 1e-09)
 
         return cam_map
@@ -74,7 +73,7 @@ class ClassActivationMaps:
         plt.imshow(cam, cmap='jet')
         plt.imshow(img, alpha=0.5)
         # save overlapping output
-        plt.savefig(f'./data/resnet34_horse_{method}.png', bbox_inches='tight')
+        plt.savefig(f'./data/resnet34_clock_{method}.png', bbox_inches='tight')
         plt.show()
 
 
@@ -82,7 +81,7 @@ class ClassActivationMaps:
         # obtain prediction for the given image
         with torch.no_grad():
             pred = self.model(t)
-        print(f'predicted class : {pred.argmax().item()}')
+        #print(f'predicted class : {pred.argmax().item()}')
         # if index is not provided use the class index with the largest logit value
         if not class_index:
             class_index = pred.argmax().item()
@@ -93,10 +92,10 @@ class ClassActivationMaps:
         weight = list(self.model.named_children())[-1][1].weight.data
 
         weight = weight.t()
-        print(cam.shape)
+        #print(cam.shape)
         print(weight.shape)
         cam = cam @ weight[:, [class_index]]
-        print(cam.shape)
+        #print(cam.shape)
         cam = cam.permute(0, 3, 1, 2)
 
         return cam
@@ -116,7 +115,7 @@ class ClassActivationMaps:
         # allow gradients for target vector
         target.require_grad=True
         # obtain loss for the class "index"
-        loss = torch.sum(pred*target)
+        loss = torch.sum(pred * target)
         # remove previous gradients
         self.model.zero_grad()
         # obtain gradients
@@ -163,13 +162,24 @@ class ClassActivationMaps:
         # the formula is simplified to just cube of the first order derivative.
         grad_3 = grad ** 3
         # get global average of gradients of each feature map
-        grad_3_mul = torch.mean(grad, (2, 3), keepdim=True)
+        grad_3_sum = torch.mean(grad, (2, 3), keepdim=True)
         # prepare for alpha denominator
-        grad_3 = grad_3 * grad_3_mul
+        grad_3 = grad_3 * grad_3_sum
         # get alpha
-        alpha_denom = 2 * grad_2 + grad_3
-        alpha_denom = torch.where(alpha_denom != 0.0, alpha_denom, torch.ones(alpha_denom.shape))
-        alpha = torch.div(grad_2, alpha_denom)
+        
+        alpha_d = 2 * grad_2 + grad_3
+        
+        torch.where(alpha_d != 0.0, alpha_d, torch.Tensor([1.0]))
+        alpha = torch.div(grad_2, alpha_d+1e-06)
+        
+        alpha_t = torch.where(relu(grad)>0, alpha, torch.Tensor([0.0]))
+        
+        alpha_c = torch.sum(alpha_t, dim=(2, 3), keepdim=True)
+        
+        alpha_cn = torch.where(alpha_c !=0, alpha_c, torch.Tensor([1.0]))
+        
+        alpha /= alpha_cn
+    
         # get final weights of each feature map
         weight = (alpha * relu(grad)).sum((2, 3), keepdim=True)
         # obtain output of last conv layer from the forward_hook
@@ -230,19 +240,19 @@ class ClassActivationMaps:
         return cam
 
 if __name__ == '__main__':
-    model = torchvision.models.alexnet(pretrained=True)
+    model = torchvision.models.resnet34(pretrained=True)
     #model = torch.hub.load('pytorch/vision:v0.6.0', 'shufflenet_v2_x1_0', pretrained=True)
-    file_name = 'dogs.jpg'
+    file_name = 'horse.jpg'
     image = read_image(os.getcwd()+'/test/'+file_name)
     cam = ClassActivationMaps(model)
     #cam.show_cam(image, method='cam')
+    #cam.show_cam(image, method='gradcam')
     cam.show_cam(image, method='gradcam')
-    cam.show_cam(image, method='gradcam++')
     #cam.show_cam(image, method='scorecam')
-    image = postprocess(image)
-    plt.gcf().set_size_inches(8, 8)
-    plt.axis('off')
-    plt.imshow(image)
-    plt.savefig('./data/'+file_name, bbox_inches='tight')
-    plt.show()
+    #image = postprocess(image)
+    #plt.gcf().set_size_inches(8, 8)
+    #plt.axis('off')
+    #plt.imshow(image)
+    #plt.savefig('./data/'+file_name, bbox_inches='tight')
+    #plt.show()
         

model/deconv_v2.py
@@ -22,7 +22,7 @@ from hook import Hook, set_hook
 
 imagenet = models.vgg16(pretrained=True)
 imagenet.eval()
-image_path = os.getcwd() + '/test/cat_dog.png'
+image_path = os.getcwd() + '/test/birds.jpg'
 image = read_image(image_path, 'imagenet')
 image.requires_grad=True
 
@@ -52,7 +52,7 @@ def get_input(input):
     # Construct "num_kernel" number of input tensor for deconv model
     dconv_input = torch.zeros_like(t)
     index = np.unravel_index(max_activation[0][2], (t.shape[2], t.shape[3]))
-    dconv_input[0, max_activation[0][0], index] = t[0, max_activation[0][0], index]
+    dconv_input[0, max_activation[0][0], index[0], index[1]] = t[0, max_activation[0][0], index[0], index[1]]
     #dconv_input[0, max_activation[i][0], index] = 1.0
     return (dconv_input, input[1], input[2])
 
@@ -65,5 +65,5 @@ loss.backward()
 print(hooks)
 for hook in hooks:
     hook.remove()
-plt.imshow(postprocess(image.grad.detach()))
+plt.imshow(postprocess(image.grad.detach()).permute(1, 2, 0))
 plt.show()

model/deconvolution_network.py
@@ -1,7 +1,7 @@
 import os
 import sys
 sys.path.insert(1, './utils')
-from utils import postprocess, clamp
+from utils import process_deconv_output, clamp, read_image
 import math
 import torch
 import torchvision
@@ -13,7 +13,7 @@ from torchvision import models
 import torch.nn.functional as F
 from collections import OrderedDict
 import matplotlib.pyplot as plt
-from read_image import read_image, tensor_to_image
+from read_image import tensor_to_image
 from torchvision.utils import make_grid, save_image
 
 class DeconvNet(nn.Module):
@@ -24,7 +24,7 @@ class DeconvNet(nn.Module):
     layer_num : layer number from which you want to perform deconvolution
     guided : set True if you want to perform guided backpropogation
     '''
-    def __init__(self, cnn, layer_num, guided=False):
+    def __init__(self, cnn, layer_number, guided=False):
 
         super().__init__()
 
@@ -32,10 +32,8 @@ class DeconvNet(nn.Module):
         self.cnn = cnn
         self.cnn.eval()
         
-        # From which layer we want to perform deconvolution
-        self.layer_num = layer_num
-
         self.guided = guided
+        self.layer_num = layer_number
 
         # Construct deconv net from input model
         self.construct_dconvnet()
@@ -125,27 +123,27 @@ class DeconvNet(nn.Module):
         # Create dict to remember indices of positive output of MaxPool and ReLU layers during forward pass
         self.pool_indices = {}
         self.relu_indices = {}
+        with torch.no_grad():
+            for layer in self.conv_model:
+                
+                # If it is MaxPool layer, remember indices of positive output
+                if isinstance(layer, nn.MaxPool2d):
+                    t, self.pool_indices[f'unpool_{pool_layer}'] = layer(t)
+                    pool_layer += 1
 
-        for layer in self.conv_model:
+                # If it is ReLU layer, remember indices of positive output
+                # Required for Guided Backpropogation only
+                elif self.guided and isinstance(layer, nn.ReLU):
+                    t = layer(t)
+                    self.relu_indices[f'unrelu_{relu_layer}'] = torch.where(t > 0, torch.tensor([1]), torch.tensor([0]))
+                    relu_layer += 1
+
+                # If it is Conv layer, perform normal forward pass
+                else:
+                    t = layer(t)
             
-            # If it is MaxPool layer, remember indices of positive output
-            if isinstance(layer, nn.MaxPool2d):
-                t, self.pool_indices[f'unpool_{pool_layer}'] = layer(t)
-                pool_layer += 1
-
-            # If it is ReLU layer, remember indices of positive output
-            # Required for Guided Backpropogation only
-            elif self.guided and isinstance(layer, nn.ReLU):
-                t = layer(t)
-                self.relu_indices[f'unrelu_{relu_layer}'] = torch.where(t > 0, torch.tensor([1]), torch.tensor([0]))
-                relu_layer += 1
-
-            # If it is Conv layer, perform normal forward pass
-            else:
-                t = layer(t)
-        
-        # return output of the specified layer number
-        return t
+            # return output of the specified layer number
+            return t
     
     def dconv_backward(self, t):
 
@@ -156,26 +154,26 @@ class DeconvNet(nn.Module):
 
         pool_layer = 1
         relu_layer = 1
+        with torch.no_grad():
+            for key, layer in self.dconv_model.named_children():
+                
+                # if it is MaxPool layer, pass the stored indices of possitive output
+                if isinstance(layer, nn.MaxUnpool2d):
+                    t = layer(t, self.pool_indices[key])
+                    pool_layer += 1
+                
+                # If it is ReLU layer, use the stored indices of forward pass of conv net
+                #  to remove negative values
+                elif self.guided and isinstance(layer, nn.ReLU):
+                    t = layer(t)
+                    t *= self.relu_indices[key]
+                    relu_layer += 1
+                    #t /= 2.
 
-        for key, layer in self.dconv_model.named_children():
-            
-            # if it is MaxPool layer, pass the stored indices of possitive output
-            if isinstance(layer, nn.MaxUnpool2d):
-                t = layer(t, self.pool_indices[key])
-                pool_layer += 1
-            
-            # If it is ReLU layer, use the stored indices of forward pass of conv net
-            #  to remove negative values
-            elif self.guided and isinstance(layer, nn.ReLU):
-                t = layer(t)
-                t *= self.relu_indices[key]
-                relu_layer += 1
-                #t /= 2.
-
-            # If it is Conv layer, perform normal forward pass on deconv model
-            else:
-                t = layer(t)
-        return t
+                # If it is Conv layer, perform normal forward pass on deconv model
+                else:
+                    t = layer(t)
+            return t
 
     def construct_dconv_input(self, t, num_kernel=9):
 
@@ -200,11 +198,13 @@ class DeconvNet(nn.Module):
         for i in range(num_kernel):
             dconv_input = torch.zeros_like(t)
             index = np.unravel_index(max_activation[i][2], (t.shape[2], t.shape[3]))
-            dconv_input[0, max_activation[i][0], index] = t[0, max_activation[i][0], index]
-            #dconv_input[0, max_activation[i][0], index] = 1.0
+            #print(index, max_activation[i][0])
+            dconv_input[0, max_activation[i][0], index[0], index[1]] = t[0, max_activation[i][0], index[0], index[1]]
+            #dconv_input[0, max_activation[i][0], index[0], index[1]] = 1.0
+            #print(torch.nonzero(dconv_input))
             yield dconv_input
     
-    def forward(self, t, num_kernel=1):
+    def get_maps(self, t, num_kernel=1):
 
         # Obtain output of the conv model
         t = self.conv_forward(t)
@@ -222,24 +222,26 @@ class DeconvNet(nn.Module):
                 maps = torch.cat((maps, out.unsqueeze(0)),dim=0)
         
         return maps
+    
+    def show_maps(self, image, output_layer, num_kernels = 1, path=False):
+
+        output = self.get_maps(image, num_kernels)
+        output_images = torch.stack([process_deconv_output(img.unsqueeze(0)) for img in output], dim=0)
+        output_maps  = torchvision.utils.make_grid(output_images, nrow=5, normalize=False)
+        plt.gcf().set_size_inches(4*num_kernels, 4)
+        plt.axis('off')
+        plt.imshow(output_maps.permute(1, 2, 0))
+        if path:
+            plt.savefig('./data/'+f'clock_deconv_output_layer_{output_layer}', bbox_inches='tight')
+            #plt.savefig(path, bbox_inches='tight')
+        plt.show()
 
 
 if __name__ == '__main__' :
 
     imagenet = models.vgg16(pretrained=True)
     image_path = os.getcwd() + '/test/clock.jpg'
-    image = read_image(image_path, 'imagenet')
-    print(image.shape)
-    output_layer = 10
-    deconvnet = DeconvNet(imagenet, output_layer, guided=True)
-    kernel = 5
-    output = deconvnet(image, kernel)
-    print(output.shape)
-    #clamp(out)
-    output_images = torch.stack([postprocess(img.unsqueeze(0)) for img in output], dim=0)
-    output_maps  = torchvision.utils.make_grid(output_images, nrow=5, normalize=True)
-    plt.gcf().set_size_inches(20, 4)
-    plt.axis('off')
-    plt.imshow(output_maps.permute(1, 2, 0))
-    plt.savefig('./data/'+f'deconv_output_layer_{output_layer}', bbox_inches='tight')
-    plt.show()
+    image = read_image(image_path)
+    layer_number=28
+    deconvnet = DeconvNet(imagenet, layer_number, guided=True)
+    output = deconvnet.show_maps(image, layer_number, 5, path=True)

utils/utils.py
@@ -1,9 +1,15 @@
 import torch
 from scipy.ndimage.filters import gaussian_filter1d
 import torchvision.transforms as transforms
+from PIL import Image, ImageEnhance
 
 mean = [0.485, 0.456, 0.406]
 std  = [0.229, 0.224, 0.225]
+
+def read_image(path):
+    image = Image.open(path).convert('RGB')
+    return preprocess(image)
+
 def preprocess(image):
 
     '''
@@ -13,20 +19,20 @@ def preprocess(image):
                 [
                     transforms.Resize(224),
                     # Reshape to size 224 X 224
-                    transforms.CenterCrop(224),
+                    #transforms.CenterCrop(224),
                     # Convert to torch tensor
                     transforms.ToTensor(),
                     # Normalize with imagenet stats
                     transforms.Normalize(
                         mean=[0.485, 0.456, 0.406],
                         std=[0.229, 0.224, 0.225]
-                    )
+                    ),
+                    transforms.Lambda(lambda x: x.unsqueeze(0))
                 ]
             )
     return transform(image)
 
-def postprocess(t):
-
+def process_deconv_output(t):
     '''
     accepts tensor of shape [B, C, H, W] processed with imagenet stats and convert it to PIL image
     '''
@@ -40,10 +46,33 @@ def postprocess(t):
             # Remove normalization using imagenet stats
             transforms.Lambda(lambda t : t*std.view(1, -1, 1, 1) + mean.view(1, -1, 1, 1)),
             # Remove batch dimention
+            transforms.Lambda(lambda t: t.squeeze(0)),
+            transforms.ToPILImage(),
+            transforms.Lambda(lambda img: ImageEnhance.Contrast(img).enhance(4)),
+            transforms.ToTensor()
+        ]
+    )
+    return transform(t)
+
+
+def postprocess(t):
+
+    '''
+    accepts tensor of shape [B, C, H, W] processed with imagenet stats and convert it to PIL image
+    '''
+
+    mean=torch.tensor([0.485, 0.456, 0.406])
+    std=torch.tensor([0.229, 0.224, 0.225])
+
+    transform = transforms.Compose(
+        [
+            # Remove normalization using imagenet stats
+            transforms.Lambda(lambda t : t*std.view(1, -1, 1, 1) + mean.view(1, -1, 1, 1)),
+            # Remove batch dimention
             #transforms.Lambda(lambda  x: (x - x.min())/(x.max() - x.min() + 1e-06)),
             transforms.Lambda(lambda t: t.squeeze(0)),
             # Convert to PIL image
-            #transforms.ToPILImage()
+            transforms.ToPILImage()
         ]
     )
     return transform(t)